085229899559503d7b1263da837fe63e761f9689
cbeveridge74 <cbeveridge74@gmail.com>
2022-11-28 15:46:45 +0000

    Cleared up the code.Creating a repo in AWS instead

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import './App.css';
-import './editor.css';
-import './site.css';
-import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-import {Schema, DOMParser} from "prosemirror-model"
-import {schema} from "prosemirror-schema-basic"
-import {addListNodes} from "prosemirror-schema-list"
-import {exampleSetup} from "prosemirror-example-setup"
-import { useEffect } from 'react';
 
 function App() {
-  useEffect( () => {
-    const mySchema = new Schema({
-      nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-      marks: schema.spec.marks
-    })
     
-    window.view = new EditorView(document.querySelector("#editor"), {
-      state: EditorState.create({
-        doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
-        plugins: exampleSetup({schema: mySchema})
-      })
-    })
-  })
-  
   return (
     <div>
-      <div id="editor" />
-      <div id="content" />
+      <div id="playground" contentEditable="true" placeholder='This is a placeholder'>This is some content</div>
     </div>
   );
 }
